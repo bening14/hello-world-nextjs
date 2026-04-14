a4ceff2d2be86bd7627cadd2b1ed3eed6d495c4d
Nindya SCM <nindya@solusiciptamedia.com>
2026-04-14 04:38:55 +0000

    Initial commit: Hello World Next.js app

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <div className="text-center">
+        <h1 className="text-6xl font-bold mb-8">
+          Hello World!
+        </h1>
+        <p className="text-xl text-gray-600">
+          This is a simple Next.js application
+        </p>
+        <p className="text-lg text-gray-500 mt-4">
+          Created by Nindya - CV Solusi Cipta Media
+        </p>
+      </div>
+    </main>
+  )
+}
